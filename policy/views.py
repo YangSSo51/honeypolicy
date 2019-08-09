a1544e569e89e8f6d022e2ea892e9fa8a4f01996
YangSSo51 <48993188+YangSSo51@users.noreply.github.com>
2019-08-10 02:21:07 +0900

    policy앱 합침,재광 pull

--- policy/views.py
+++ policy/views.py
@@ -36,11 +36,8 @@ def create(request):
     
 # 게시글 읽어와유~
 ''' def read(request):
-
     # policyCount = PolicyList.objects.count()
-
     policies = PolicyList.objects.all()
-
     # 검색을 할 경우를 나눈것!
     # posts 로 변수 통일 함 -> 템플릿에서 값 하나만 사용해서 나타내다 보니..ㅠ
     q = request.GET.get('q','')
@@ -62,9 +59,9 @@ def create(request):
             posts = paginator.page(1)
         except EmptyPage:
             posts = paginator.page(paginator.num_pages)
-
     
     return render(request,'policyList.html' , {'policies':policies, 'posts':posts, 'policyCount':policyCount}) '''
+# 게시글 읽어와유~
 def read(request):
 
     # policyCount = PolicyList.objects.count()
@@ -73,24 +70,35 @@ def read(request):
 
     # 검색을 할 경우를 나눈것!
     # posts 로 변수 통일 함 -> 템플릿에서 값 하나만 사용해서 나타내다 보니..ㅠ
-    q = request.GET.get('q','')
-
-    r = request.GET.get('region','')
-
-    if q and not r:
-        policies = policies.filter(title__contains=q)
-        policyCount = policies.filter(title__contains=q).count()
-    elif not q and r:
-        policies = policies.filter(region__contains=r)
-        policyCount = policies.filter(region__contains=r).count()
+    
+    policyCount = PolicyList.objects.count()
+    page = request.GET.get('page',1)
+    paginator = Paginator(policies,6)
+    try:
+        policies = paginator.page(page)
+    except PageNotAnInteger:
+        policies = paginator.page(1)
+    except EmptyPage:
+        policies = paginator.page(paginator.num_pages)
+    
+    index = policies.number - 1
+    max_index = len(paginator.page_range)
+    start_index = index -2 if index >= 2 else 0
+    if index < 2:
+        end_index = 5 - start_index
     else:
-        policies = policies.filter(title__contains=q, region__contains=r)
-        policyCount = policies.filter(title__contains=q, region__contains=r).count()
+        end_index = index + 3 if index <= max_index -3 else max_index
+    
+    page_range = list(paginator.page_range[start_index:end_index])
+
+    context = { 'policies': policies , 'page_range':page_range, 'max_index':max_index-2 }
+
 
     # policies = policies.filter(title__contains=q, region__contains=r).order_by('-hits')   
     # policyCount = policies.filter(title__contains=q).count()
 
-    return render(request,'policyList.html' , {'policies':policies, 'policyCount':policyCount})
+    # return render(request,'policyList.html' , {'policies':policies, 'policyCount':policyCount})
+    return render(request,'policyList.html' , context)
 
 def update(request,pk):
     # 어떤 게시글을 수정할지 가져오기
@@ -98,13 +106,28 @@ def update(request,pk):
 
     # 해당하는 글 객체 pk에 맞는 입력공간
     # 위에서 가져온 policyList 객체를 인자로 넣어주기
-    form = NewPolicyList(request.POST, instance=policy)
-    if form.is_valid():
-        form.save()
-        return redirect('policyread')
+    # form = NewPolicyList(request.POST, instance=policy)
+    # if form.is_valid():
+    #     form.save()
+    #     return redirect('policyread')
     
     # 다른 request 인경우??
-    return render(request, 'newPolicyList.html', {'form':form})
+    return render(request, 'updatePolicy.html', {'policy':policy})
+
+#실질적인 수정은 여기서 동작,이때 pk값은 update에서 넘겨준 id(notice.id)값으로 가짐    
+def modify(request):
+    if request.method == 'POST':
+        policy = get_object_or_404(PolicyList,pk = request.POST['id'])
+        policy.title = request.POST['title']
+        policy.body = request.POST['body']
+        policy.region = request.POST['region']
+        policy.age = request.POST['age']
+        policy.start_date = request.POST['start_date']
+        policy.end_date = request.POST['end_date']
+        policy.educated = request.POST['educated']
+        policy.save()
+        #notice.update(pk = notice_id)
+    return redirect('policyread')  
 
 def regist(request,pk):
     Cal = apps.get_model('cal', 'Event')
@@ -130,44 +153,91 @@ def detail(request, policy_id):
     policyDetail = get_object_or_404(PolicyList, pk=policy_id)
     return render(request, 'detailPolicy.html', {'policyDetail':policyDetail, 'policy_id' : policy_id})
 
+
 def search(request):
     policies = PolicyList.objects.all()
 
     # 상단에 검색하는 부분 
+    # 제목이나 내용으로 검색하기
     searchTop = request.GET.get('searchTop','')
     
-    # 지역 - 서울 인천 경기 
-    region = request.GET.get('region','')
+    # 정책 상세보기 페이지에서 검색
+    searchPolicy = request.GET.get('searchPolicy','')
 
-    # 대상 - 청년 여성 중장년 무관
-    target = request.GET.get('target','')
-
-    # 학력 - 중졸 고졸 대졸 무관
-    educated = request.GET.get('educated','')
-
-
-    if search:
+    if searchTop:
         # policies = policies.filter(title__contains=region)   
-        policies = PolicyList.objects.filter(Q(title__icontains=searchTop) | Q(body__icontains=searchTop)) 
+        # policies = PolicyList.objects.filter(Q(title__icontains=searchTop) | Q(body__icontains=searchTop)) 
+        policies = PolicyList.objects.filter(body__icontains=searchTop) 
+    elif searchPolicy:
+        rangePolicy = request.GET.get('rangePolicy','')
+
+        # 범위 x 검색만 -> 내용으로만 검색 하기
+        if searchPolicy and not rangePolicy:
+            policies = policies.filter(body__contains=searchPolicy)
+            policyCount = policies.filter(body__contains=searchPolicy).count()
+        # 범위 선택 , 검색 -> 범위에 맞는 검색 하기
+        elif searchPolicy and rangePolicy:
+            # 제목 검색
+            if rangePolicy == 'title':
+                policies = policies.filter(title__contains=searchPolicy)
+                # policyCount = policies.filter(title__contains=searchPolicy).count()
+            # 내용 검색
+            elif rangePolicy == 'body':
+                policies = policies.filter(body__contains=searchPolicy)
+                # policyCount = policies.filter(body__contains=searchPolicy).count()
+            # 지역 검색
+            elif rangePolicy == 'region':
+                policies = policies.filter(region__contains=searchPolicy)
+                # policyCount = policies.filter(region__contains=searchPolicy).count()
     else:
+
+        # 지역 - 서울 인천 경기 
+        region = request.GET.get('region','')
+
+        # 대상 - 청년 여성 중장년 무관
+        target = request.GET.get('target','')
+
+        # 학력 - 중졸 고졸 대졸 무관
+        educated = request.GET.get('educated','')
+
+        # start_date = request.GET.get('start_date','')
+        # end_date = request.GET.get('end_date','')
+
+        # 시작일 , 마감일 -> 리스트로 만들기
+        # date_list = list(start_date,end_date)
+
+        # 검색 리스트 
+        # 지역 , 대상 , 학력 , 기간 
+        # search_list = list(region, target, educated, date_list)
+
+        # 리스트에서 값이 있는 것만 뽑기 
+        # for n in search_list:
+        #     result_list = []
+        #     if n:
+        #         result_list.append(n)
+            
+        
         # 지역
         if region and not target and not educated:
             policies = policies.filter(region__contains=region)   
         # 지역 + 대상
+        # 대상을 내용에서 찾기
         elif region and target and not educated:
-            policies = policies.filter(region__contains=region, target__contains=target)
+            policies = policies.filter(region__contains=region, body__contains=target)
+        
         # 지역 + 학력 
         elif region and not target and educated:
             policies = policies.filter(region__contains=region, educated__contains=educated)
         # 지역 + 대상 + 학력
         elif region and target and educated:
-            policies = policies.filter(region__contains=region, target__contains=target, educated__contains=educated)
+            policies = policies.filter(region__contains=region, body__contains=target, educated__contains=educated)
+
         # 대상
         elif not region and target and not educated:
-            policies = policies.filter(target__contains=target)
+            policies = policies.filter(body__contains=target)
         # 대상 + 학력 
         elif not region and target and educated:
-            policies = policies.filter(target__contains=target, educated__contains=educated)
+            policies = policies.filter(body__contains=target, educated__contains=educated)
         # 학력
         elif not region and not target and educated:
             policies = policies.filter(educated__contains=educated)
@@ -176,4 +246,27 @@ def search(request):
             # 그냥 써놓은....
             pass
 
-    return render(request, 'policyList.html', {'policies':policies})
+    page = request.GET.get('page',1)
+    paginator = Paginator(policies,6)
+    try:
+        policies = paginator.page(page)
+    except PageNotAnInteger:
+        policies = paginator.page(1)
+    except EmptyPage:
+        policies = paginator.page(paginator.num_pages)
+    
+    index = policies.number - 1
+    max_index = len(paginator.page_range)
+    start_index = index -2 if index >= 2 else 0
+    if index < 2:
+        end_index = 5 - start_index
+    else:
+        end_index = index + 3 if index <= max_index -3 else max_index
+    
+    page_range = list(paginator.page_range[start_index:end_index])
+
+    context = { 'policies': policies , 'page_range':page_range, 'max_index':max_index-2 }
+
+    return render(request,'policyList.html' , context)
+    # return render(request, 'policyList.html', {'policies':policies})
+
