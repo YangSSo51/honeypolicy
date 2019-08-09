7a456127b2acbacd5cc1e07fc21fb42900f492e6
YangSSo51 <48993188+YangSSo51@users.noreply.github.com>
2019-08-10 00:42:14 +0900

    age length 300으로

--- policy/models.py
+++ policy/models.py
@@ -28,7 +28,7 @@ class PolicyList(models.Model):
     region = models.CharField(max_length=20)
 
     # 연령
-    age = models.CharField(max_length=120)
+    age = models.CharField(max_length=300)
 
     # 정책 시작 기간
     start_date = models.DateField(null=True)
